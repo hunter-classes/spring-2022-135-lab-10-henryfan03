0aa1db9b7b8b906d60c06ebf83a47db5befd40dc
Henry Fan <henry.fan2313@gmail.com>
2022-04-08 03:13:28 -0400

    added test cases

--- tests.cpp
+++ tests.cpp
@@ -36,6 +36,14 @@ TEST_CASE("addMinutes") {
   Time b = addMinutes({8, 10}, 75);
   CHECK(b.h == a.h);
   CHECK(b.m == a.m);
+  Time c = {0,30};
+  Time d = addMinutes({0, 0}, 30);
+  CHECK(c.h == d.h);
+  CHECK(c.m == d.m);
+  Time e = {1,25};
+  Time f = addMinutes({0, 10}, 75);
+  CHECK(e.h == f.h);
+  CHECK(e.m == f.m);
 }
 
 TEST_CASE("GetMovie") {
@@ -54,4 +62,27 @@ TEST_CASE("GetDuration") {
   TimeSlot daytime = {movie2, {12, 15}};
   TimeSlot evening = {movie2, {16, 45}};
   CHECK(getTimeSlot(morning) == "[starts at 9:15, ends by 11:11]");
+}
+
+TEST_CASE("Scheduling X after Y") {
+  Movie movie1 = {"Back to the Future", COMEDY, 116};
+  Movie movie2 = {"Black Panther", ACTION, 134};
+  TimeSlot morning = {movie1, {9, 15}};
+  TimeSlot daytime = {movie2, {12, 15}};
+  TimeSlot evening = {movie2, {16, 45}};
+  Time expected = {14,29};
+  TimeSlot resultTS = scheduleAfter(daytime,movie2);
+  Time result = resultTS.startTime;
+  CHECK(result.h == expected.h);
+  CHECK(result.m == expected.m);
+}
+
+TEST_CASE("Overlapping time slots") {
+  Movie movie1 = {"Back to the Future", COMEDY, 116};
+  Movie movie2 = {"Black Panther", ACTION, 134};
+  TimeSlot morning = {movie1, {9, 15}};
+  TimeSlot daytime = {movie2, {12, 15}};
+  TimeSlot evening = {movie2, {16, 45}};
+  CHECK(timeOverlap(morning,daytime) == false);
+  CHECK(timeOverlap(morning,morning) == true);
 }
